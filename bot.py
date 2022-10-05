9957ec236073d1562476de1be7ee36edc80a4440
danyaorlov4 <bigworld22@mail.ru>
2022-10-06 00:06:47 +0300

    Add class and start async update stat

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 import os
+import asyncio
 from utils.sheet_connect import *
 from buttons.but import keybrd
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from utils.person import Person
 
+storage = MemoryStorage()
 
 bot = Bot(token=os.environ["TOKEN"])
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 
 names = {
     "orloffdd": "Орлов Даниил",
@@ -17,17 +22,40 @@ names = {
 
 
 @dp.message_handler(commands=["start"])
-async def command_start(message: types.Message):
+async def command_start(message: types.Message, state: FSMContext):
+
+    name = names.get(message.from_user.username)
     await message.answer(
         "Привет! Я - бот, призванный сделать процесс твоего поиска интереснее и веселее. Давай начнем!",
         reply_markup=keybrd,
     )
 
+    global stop_polling_sheet
+    stop_polling_sheet = asyncio.Event()
+    while True:
+        try:
+            await asyncio.wait_for(stop_polling_sheet.wait(), timeout=10)
+        except asyncio.TimeoutError:
+            try:
+                stat = await object_create(name)
+                async with state.proxy() as data:
+                    data["person"] = Person(
+                        stat[0], stat[1], stat[2], stat[3], stat[4], stat[5], stat[6]
+                    )
+            except:
+                await message.answer("not working")
+
+
+@dp.message_handler(text="test")
+async def test(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        await message.answer(data["person"].get_founded())
+
 
 @dp.message_handler(text="📊Статистика")
 async def stat_send(message: types.Message):
     msg = await message.answer("Запрос обрабатывается, подожди...")
-    stat = await get_stat(names.get(message.from_user.username), "stat")
+    stat = await get_statistic(names.get(message.from_user.username), "stat")
     await msg.edit_text(stat)
 
 
--- utils/sheet_connect.py
+++ utils/sheet_connect.py
@@ -1,7 +1,40 @@
+from tkinter.font import names
 from . import sh
+from .person import Person
 
 
-async def get_stat(name, type):
+async def object_create(name):
+
+    current_lvl = 0
+    current_score = 0
+    max_score_on_level = 0
+
+    wks = sh.worksheet_by_title("Лист6")
+    names_col = wks.get_col(1)
+
+    for i in range(len(names_col)):
+        if name == names_col[i]:
+            person_statistic = wks.get_row(i + 1, include_tailing_empty=False)
+
+    lvl_sh = sh.worksheet_by_title("lvl")
+    levels = lvl_sh.get_col(1, include_tailing_empty=False)
+
+    found = int(person_statistic[2])
+    for i in range(len(levels)):
+        if found < int(levels[i]):
+            current_lvl = i + 1
+            current_score = (found % int(levels[i - 1])) * 7
+            max_score_on_level = (int(levels[i]) - int(levels[i - 1])) * 7
+            break
+
+    person_statistic.append(str(current_lvl))
+    person_statistic.append(str(current_score))
+    person_statistic.append(str(max_score_on_level))
+
+    return person_statistic
+
+
+async def get_statistic(name, type):
 
     wks = sh.worksheet_by_title("Лист6")
     stat = wks.get_all_records()
@@ -27,7 +60,7 @@ async def get_stat(name, type):
 
 async def get_level(name):
 
-    person_statistic = await get_stat(name=name, type="lvl")
+    person_statistic = await get_statistic(name=name, type="lvl")
 
     current_lvl = 0
     current_score = 0
